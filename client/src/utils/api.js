a1e5df8b2477c8e6e118f5ff599c76d46f51d2c6
left0077 <10624624+left0077@user.noreply.gitee.com>
2026-05-03 11:52:34 +0800

    config: update default backend to https://ai.r99053000.nyat.app:59745

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 const STORAGE_KEY = "freeroll_backend_url";
 
 export function getBackendUrl() {
-  return localStorage.getItem(STORAGE_KEY) || "https://frp-cat.com:59745";
+  return localStorage.getItem(STORAGE_KEY) || "https://ai.r99053000.nyat.app:59745";
 }
 
 export function setBackendUrl(url) {
